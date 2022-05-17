6411c629aa269c72116a8614f23c6758e5d86fb6
Haxersociety <Chelgames12@gmail.com>
2022-05-17 13:15:56 +0500

    Написал метод парсинга сайти, и спарсил с него задания.

---
+++ app/src/main/java/com/chelgames/egeintheroad/crbd.kt
@@ -0,0 +1,2 @@
+package com.chelgames.egeintheroad
+
---
+++ app/src/main/java/create_db.kt
@@ -0,0 +1,37 @@
+import android.os.Build
+import androidx.annotation.RequiresApi
+import org.jsoup.Jsoup
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun main() {
+
+    val ege_number_id = arrayOf(
+        258, 289, 290, 202, 286, 337, 287, 206, 352, 344,
+        343, 346, 302, 303, 297, 307, 305, 306, 309, 310,
+        312, 313, 314, 321, 322, 323
+    )
+
+    var group = 0
+
+    for(id in ege_number_id) {
+        val rootDir = Paths.get("app/src/main/assets")
+        group++
+        val dir = Files.createTempDirectory(rootDir, "rus_${group}_")
+
+        val doc =
+            Jsoup.connect("https://rus-ege.sdamgia.ru/test?filter=all&category_id=${id}&print=true&ans=true")
+                .get()
+        val elems = doc.getElementsByAttributeValue("class", "prob_maindiv")
+        println(elems.size)
+        for(i in 0 until elems.size) {
+            val File_name = "$i.html"
+            var text = elems[i].toString()
+            val fos = File("$dir/$File_name")
+            fos.writeText(text)
+        }
+
+    }
+}
